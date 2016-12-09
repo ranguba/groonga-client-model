aa208ee46c73f0dd0d885e03281d69230f6c92f5
Kouhei Sutou <kou@clear-code.com>
2016-12-09 18:24:14 +0900

    Create real pseudo column

--- lib/groonga_client_model/schema.rb
+++ lib/groonga_client_model/schema.rb
@@ -29,57 +29,70 @@ module GroongaClientModel
     end
 
     def tables
-      Tables.new(@raw.tables)
+      Tables.new(@raw, @raw.tables)
     end
 
     class Tables
-      def initialize(raw)
-        @raw = raw
+      def initialize(raw_schema, raw_tables)
+        @raw_schema = raw_schema
+        @raw_tables = raw_tables
       end
 
       def [](name)
         name = name.to_s if name.is_a?(Symbol)
-        raw_table = @raw[name]
+        raw_table = @raw_tables[name]
         raise Error, "table doesn't exist: <#{name.inspect}>" if raw_table.nil?
-        Table.new(raw_table)
+        Table.new(@raw_schema, raw_table)
       end
 
       def exist?(name)
-        @raw.key?(name)
+        @raw_tables.key?(name)
       end
     end
 
     class Table
-      def initialize(raw)
-        @raw = raw
+      def initialize(raw_schema, raw_table)
+        @raw_schema = raw_schema
+        @raw_table = raw_table
       end
 
       def name
-        @raw.name
+        @raw_table.name
       end
 
       def columns
-        raw_columns = @raw.columns.merge("_id" => {"name" => "_id"})
-        if @raw.key_type
-          raw_columns = raw_columns.merge("_key" => {"name" => "_key"})
+        raw_columns = {}
+        raw_columns["_id"] = create_pseudo_column("_id")
+        if @raw_table.key_type
+          raw_columns["_key"] = create_pseudo_column("_key")
         end
-        Columns.new(raw_columns)
+        Columns.new(@raw_schema, @raw_table.columns.merge(raw_columns))
+      end
+
+      private
+      def create_pseudo_column(name)
+        raw_column = {
+          "name" => name,
+          "indexes" => [],
+        }
+        Groonga::Client::Response::Schema::Column.new(@raw_schema, raw_column)
       end
     end
 
     class Columns
       include Enumerable
 
-      def initialize(raw)
-        @raw = raw
+      def initialize(raw_schema, raw_columns)
+        @raw_schema = raw_schema
+        @raw_columns = raw_columns
       end
 
       def names
-        @raw.keys
+        @raw_columns.keys
       end
 
       def each
-        @raw.each do |name, column|
+        @raw_columns.each do |name, column|
           yield(name, column)
         end
       end
