aaa2531c0ef3d3576b6d7302669a4337c50ce83a
Kouhei Sutou <kou@clear-code.com>
2017-01-12 11:29:56 +0900

    Bump version

--- lib/groonga_client_model/version.rb
+++ lib/groonga_client_model/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2016  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2016-2017  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,5 +15,5 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module GroongaClientModel
-  VERSION = "0.9.6"
+  VERSION = "0.9.7"
 end
