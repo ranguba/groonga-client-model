c35b9b66beba0a3fb3ef0f6c8ebe5542d7439492
Kouhei Sutou <kou@clear-code.com>
2017-02-06 16:56:20 +0900

    test: fix expected

--- test/apps/rails5/test/models/age_test.rb
+++ test/apps/rails5/test/models/age_test.rb
@@ -7,9 +7,13 @@ class AgeTest < ActiveSupport::TestCase
     key = "Hello"
     age = Age.new(_key: key)
     assert(age.invalid?)
-    assert_equal([age.errors.generate_message(:_key,
-                                              :not_a_positive_integer,
-                                              inspected_value: key.inspect)],
-                 age.errors.full_messages)
+    assert_equal({
+                   _key: [
+                     age.errors.generate_message(:_key,
+                                                 :not_a_positive_integer,
+                                                 inspected_value: key.inspect)
+                   ],
+                 },
+                 age.errors.messages)
   end
 end
