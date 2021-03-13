73b6b089ecf5786bbbcb781170c22b6b565d2d72
Sutou Kouhei <kou@clear-code.com>
2021-03-13 15:50:57 +0900

    test: add test app generator

---
+++ test/apps/add.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Copyright (C) 2021  Sutou Kouhei <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 RAILS_VERSION [OPTIONS...]"
+  echo " e.g.: $0 6.2"
+  exit 1
+fi
+
+set -eux
+
+rails_version=$1
+shift
+
+rm -rf blog
+rails _${rails_version}_ new blog \
+      --skip-active-record \
+      --skip-spring
+pushd blog
+cat <<GEMFILE >> Gemfile
+gem 'groonga-client-model', path: '../../../'
+gem 'groonga-client', path: '../../../../groonga-client'
+
+group :development, :test do
+  gem 'factory_bot_rails'
+end
+GEMFILE
+bin/bundle update
+sed -i'' -e 's/"yarn"/"yarnpkg", "yarn"/g' bin/yarn
+PATH=$PWD/bin:$PATH rails webpacker:install
+bin/rails generate scaffold post title:string body:text
+cat <<TEST_HELPER >> test/test_helper.rb
+
+require "groonga_client_model/test_helper"
+class ActiveSupport::TestCase
+  include GroongaClientModel::TestHelper
+  include FactoryBot::Syntax::Methods
+end
+TEST_HELPER
+sed -i'' -e 's/posts(:one)/create(:post)/g' test/**/*_test.rb
+PATH=$PWD/bin:$PATH rails test:all
+sed -i'' -e 's/^ruby .*//g' Gemfile
+rm .ruby-version
+rm Gemfile.lock
+rm yarn.lock
+echo <<GITIGNORE >> .gitignore
+
+/.ruby-version
+/Gemfile.lock
+/yarn.lock
+GITIGNORE
+rm -rf .git
+popd
+mv blog rails${rails_version}
+git add rails${rails_version}
